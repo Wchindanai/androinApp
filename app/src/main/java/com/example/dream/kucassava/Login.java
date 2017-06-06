c7ba9949ff40a9d47a972a93eec0bc25d63f6042
Chindanai Wasanachin <dn.chindanai@gmail.com>
2017-06-06 23:01:53 +0700

    add login and register

--- app/src/main/java/com/example/dream/kucassava/Login.java
+++ app/src/main/java/com/example/dream/kucassava/Login.java
@@ -1,13 +1,26 @@
 package com.example.dream.kucassava;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+
+    TextView forgetBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        forgetBtn = (TextView) findViewById(R.id.forgetPassword);
+        forgetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplication(), ForgetPassword.class));
+            }
+        });
     }
 }
